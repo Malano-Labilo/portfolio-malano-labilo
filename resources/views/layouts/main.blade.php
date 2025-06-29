27cc924531293c1b0b06119fc8259cf4f4591704
malano-labilo <mabilzakme@gmail.com>
2025-06-29 20:56:38 +0700

    minim

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BGNq0W5d.css') }}">
 </head>
 
 <body class="scroll-smooth bg-white-first font-aleo text-dark-first text-[14px]">
     <x-header />
     @yield('content')
     <x-footer />
+    <script src={{ asset('build/assets/app-DaBYqt0m.js') }} defer></script>
     @vite('resources/js/app.js')
 </body>
 
--- start.sh
+++ start.sh
@@ -3,10 +3,5 @@
 echo "📦 Running migrations..."
 php artisan migrate --force || true
 
-php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
-php artisan route:clear
-
 echo "🚀 Starting Laravel server..."
 exec php artisan serve --host=0.0.0.0 --port=8080

0ddbbac26aa2db74b0360383f825e449a1177f23
malano-labilo <mabilzakme@gmail.com>
2025-06-29 20:10:06 +0700

    minim

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 echo "📦 Running migrations..."
 php artisan migrate --force || true
 
+npm install && npm run build
+
 echo "🚀 Starting Laravel server..."
 exec php artisan serve --host=0.0.0.0 --port=8080

f48ca0aeae9883ad178ae5ab826b232a328ee273
malano-labilo <mabilzakme@gmail.com>
2025-06-30 08:34:24 +0700

    menambahkan mekanisme https di app\Providers\AppServiceProvider.php 30 Juni 2025 08:34

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(env('APP_ENV') === 'production') {
+            // Force HTTPS in production environment
+            URL::forceScheme('https');
+        }
         //Jika ada yang menggunakan lazy loading akan error
         Model::preventLazyLoading();
 
